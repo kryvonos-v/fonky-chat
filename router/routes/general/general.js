f247b74d82c51a9cbffaf2e8a54b25a36076fe5c
Volodymyr Kryvonos <volodymyr.kryvonos@gmail.com>
2020-08-27 21:39:51 +0300

    Created definition for general app routes

--- router/routes/general/general.js
+++ router/routes/general/general.js
@@ -4,7 +4,12 @@ async function homePage(ctx) {
   ctx.body = render('views/index.html');
 }
 
+async function userProfilePage(ctx) {
+  ctx.body = render('views/user-profile.html');
+}
+
 exports.setUp = router => {
   router.get('/', homePage);
+  router.get('/profile', userProfilePage);
 };
 
