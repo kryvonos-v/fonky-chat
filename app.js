7c65a6dfd7a44a9e90124ad99670b7ab14214905
Volodymyr Kryvonos <volodymyr.kryvonos@gmail.com>
2020-08-27 21:37:30 +0300

    Use koa-session

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const Koa = require('koa');
+const session = require('koa-session');
 const { router } = require('./router/router');
 
 const app = new Koa();
 const PORT = 3000;
+const ENV = process.env.NODE_ENV;
 
+app.keys = ['some secret key'];
+
+app.use(session({
+  maxAge: 86400000,
+  secure: false
+}, app));
 app.use(router.routes());
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
